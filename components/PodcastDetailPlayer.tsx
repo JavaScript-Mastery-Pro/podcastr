65d346571c8029788abc57ece84ae9c515402157
anil-jsmastery <anil@jsmastery.pro>
2024-05-07 21:01:31 +0545

    max width for play button

--- components/PodcastDetailPlayer.tsx
+++ components/PodcastDetailPlayer.tsx
@@ -90,7 +90,7 @@ const PodcastDetailPlayer = ({
 
           <Button
             onClick={handlePlay}
-            className="text-16 bg-orange-1 font-extrabold text-white-1"
+            className="text-16 w-full max-w-[250px] bg-orange-1 font-extrabold text-white-1"
           >
             <Image
               src="/icons/Play.svg"
